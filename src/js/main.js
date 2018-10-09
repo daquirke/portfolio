268441ae8691be5269d1e6496042b0f01ca7796d
daquirke <david.qrk@gmail.com>
2018-10-09 15:05:26 +0800

    Initalize project

---
+++ src/js/main.js
@@ -0,0 +1,113 @@
+/* skills bar */
+
+
+$(window).scroll(function() {
+  var hT = $('#skill-bar-wrapper').offset().top,
+    hH = $('#skill-bar-wrapper').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+  if (wS > (hT+hH-1.4*wH)){
+    jQuery(document).ready(function(){
+      jQuery('.skillbar-container').each(function(){
+        jQuery(this).find('.skills').animate({
+          width:jQuery(this).attr('data-percent')
+        }, 5000); // 3 seconds
+      });
+    });
+  }
+});
+
+/* TYPING HEADER */
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+
+  // ANIMATE CSS ONHOVER
+
+  function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+      function() {
+        element.addClass('animated ' + animation);
+      },
+      function(){
+        //wait for animation to finish before removing classes
+        window.setTimeout( function(){
+          element.removeClass('animated ' + animation);
+        }, 2000);
+      });
+  }
+
+  $(document).ready(function(){
+    $('#cool').each(function() {
+      animationHover(this, 'bounce');
+    });
+  });
+
+
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+}
+
+/* contact form */
+
+$('input').on('focusin', function() {
+  $(this).parent().find('label').addClass('active');
+});
+
+$('input').on('focusout', function() {
+  if (!this.value) {
+    $(this).parent().find('label').removeClass('active');
+  }
+});
